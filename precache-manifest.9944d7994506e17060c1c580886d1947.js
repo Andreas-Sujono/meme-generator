81f0abfcebc2b7a417186cba3916320de20dcf98
Andreas_Sujono <andreassujono@gmail.com>
2020-02-17 02:26:13 +0800

    Updates

---
+++ precache-manifest.9944d7994506e17060c1c580886d1947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a542ba6d359e7d2551229aaa59d2ce16",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "c0bab67f951bbe9d59c2",
+    "url": "/meme-generator/static/css/main.ef4e046c.chunk.css"
+  },
+  {
+    "revision": "38e9f0d683c5566a295a",
+    "url": "/meme-generator/static/js/2.a914a69a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/meme-generator/static/js/2.a914a69a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0bab67f951bbe9d59c2",
+    "url": "/meme-generator/static/js/main.dc218e29.chunk.js"
+  },
+  {
+    "revision": "2a81abe5fb348515627e",
+    "url": "/meme-generator/static/js/runtime-main.de1c8fd4.js"
+  }
+]);
